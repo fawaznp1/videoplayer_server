aa0f1be6950954d6992bed79bcbfe92b6ac7c67b
fawaz <fawaznp3598@gmail.com>
2023-11-10 13:57:22 +0530

    videoserver

---
+++ index.js
@@ -0,0 +1,27 @@
+//import json-server library in index.js
+const jsonServer = require('json-server')
+
+//create server using json-server library
+const mediaPlayerServer=jsonServer.create()
+
+//create a path to db.json
+const router=jsonServer.router('db.json')
+
+//middlewear to convert js to json
+const middlewear=jsonServer.defaults()
+
+//connect
+mediaPlayerServer.use(middlewear)
+mediaPlayerServer.use(router)
+
+//setup port for server
+const port = 5000 || process.env.PORT 
+
+//monitor 4000 
+mediaPlayerServer.listen(port,()=>
+{
+    console.log(`mediaPlayerServer is listening to ${port} and waiting `);
+})
+
+
+
